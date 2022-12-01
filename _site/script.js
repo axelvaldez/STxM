e43065480adc6d5ca39c936f2af8ca9ea313240e
Axel Valdez <axel@paskola.com>
2022-11-30 19:08:07 -0700

    ignore _site

--- _site/script.js
+++
@@ -1,9 +0,0 @@
-const imgs = document.querySelectorAll('img');
-
-function imgClickHandler(){
-    this.classList.toggle('active');
-}
-
-imgs.forEach((img) => {
-    img.addEventListener('click', imgClickHandler);
-});
